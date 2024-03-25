f1b950851b3ef16dec0060209f388befb7fe3e17
arttuvee <arttuvo@metropolia.fi>
2024-03-25 19:21:04 +0200

    combine main.js and map.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Function to open user info dropdown in header
 function toggleDropdown(event) {
   const dropdown = document.getElementById('dropdown');
   dropdown.style.display = dropdown.style.display !== 'flex' ?
@@ -6,13 +7,21 @@ function toggleDropdown(event) {
   event.stopPropagation();
 }
 
-// Add a click event listener to the document
+// Click event listener to close the dropdown when clicked outside of it
 document.addEventListener('click', function() {
   const dropdown = document.getElementById('dropdown');
   dropdown.style.display = 'none'; // Hide the dropdown
 });
 
-
+// Function to open the filter window for the map
 document.querySelector('#settings-icon').addEventListener('click', function() {
   document.querySelector('.filter-window').classList.toggle('open');
 });
+
+// Leaflet map
+const map = L.map('leaflet-map').setView([60.21, 24.94], 11);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
