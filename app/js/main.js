42fe159d596558bce5e17252ba99c0076ad80a7d
arttuvee <arttuvo@metropolia.fi>
2024-04-11 17:13:13 +0300

    Change website layout and design

--- app/js/main.js
+++ app/js/main.js
@@ -13,15 +13,9 @@ document.addEventListener('click', function() {
   dropdown.style.display = 'none'; // Hide the dropdown
 });
 
-// Function to open the filter window for the map
-document.querySelector('#settings-icon').addEventListener('click', function() {
-  document.querySelector('.filter-window').classList.toggle('open');
-});
-
 // Leaflet map
 const map = L.map('leaflet-map').setView([60.21, 24.94], 11);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-
